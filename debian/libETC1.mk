1fe53808342b6e30650581dffff861a24a9317ef
Kai-Chung Yan <seamlikok@gmail.com>
2016-02-12 01:55:40 +0800

    Only build indep packages on other architectures

--- debian/libETC1.mk
+++ debian/libETC1.mk
@@ -8,6 +8,7 @@ LDFLAGS += -shared -Wl,-soname,$(NAME).so.0
 $(NAME).so.0: $(SOURCES)
 	$(CXX) $^ -o $@ $(CXXFLAGS) $(CPPFLAGS) $(LDFLAGS)
 	ln -s $(NAME).so.0 $(NAME).so
+	dpkg-gensymbols -pandroid-libetc1 -v0 -e$@ -Odebian/android-libetc1.symbols
 
 clean:
 	$(RM) $(NAME).so*
